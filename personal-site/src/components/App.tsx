f70bf53a435036658e75dc84d87f9d5e63c2a017
Ghufran <ghufranshahid101@gmail.com>
2023-07-08 16:35:33 -0400

    Edited styling for introduction divs

--- personal-site/src/components/App.tsx
+++ personal-site/src/components/App.tsx
@@ -9,7 +9,15 @@ function App() {
   return (
     <div className="p-2">
       <Header></Header>
-      <div className="flex justify-center">Hello World</div>
+      <div className="flex min-h-[93vh] flex-col lg:flex-row">
+        <div className="flex self-center flex-col min-w-[50%]">
+          Hello, I'm
+          <div>Ghufran Shahid</div>
+        </div>
+        <div className='flex self-center min-w-[50%]'>
+          And welcome to my website.
+        </div>
+      </div>
     </div>
   )
 }
