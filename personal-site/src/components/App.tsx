415dabd51c8d4784f30ce089d5c261483d77fc43
Ghufran <ghufranshahid101@gmail.com>
2023-07-09 13:16:16 -0400

    Added styling and rearranged order of components in introductory section of page

--- personal-site/src/components/App.tsx
+++ personal-site/src/components/App.tsx
@@ -9,14 +9,20 @@ function App() {
   return (
     <div className="p-2">
       <Header></Header>
-      <div className="flex min-h-[93vh] flex-col lg:flex-row">
-        <div className="flex self-center flex-col min-w-[50%]">
-          Hello, I'm
-          <div>Ghufran Shahid</div>
+      <div className="flex min-h-[93vh] flex-col justify-start text-3xl md:flex-row xl:text-6xl lg:text-5xl p-6 font-bold gap-1">
+
+        {/*/Introductory Divs*/}
+
+        <div className="flex md:items-center md:justify-center flex-col min-w-[50%] gap-1">
+          <div>
+            Hey, I'm
+            <div className="animate-fade-up animate-delay-500 animate-duration-1000 animate-once">Ghufran Shahid.</div>
+          </div>
         </div>
-        <div className='flex self-center min-w-[50%]'>
-          And welcome to my website.
+        <div className='flex md:items-center min-w-[50%] animate-fade-down animate-delay-500 animate-duration-1000 md:justify-center'>
+          Welcome to my website.
         </div>
+
       </div>
     </div>
   )
