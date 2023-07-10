65437cba90bb5f685a1a03b241083af5be95fc04
Ghufran <ghufranshahid101@gmail.com>
2023-07-09 21:39:02 -0400

    Added icon buttons for social media outlets, fixed position to create sticky bar

--- personal-site/src/components/header.tsx
+++ personal-site/src/components/header.tsx
@@ -3,8 +3,15 @@
 function Header(){
 
     return(
-        <div className="flex justify-end min-h-[7vh]">
-            Header
+        <div className=" w-full justify-end fixed top-0 border-b-slate-50 border-b-4 bg-white z-10 p-4">
+            <div className="flex justify-end gap-5">
+                <button onClick={() => {window.open("https://www.linkedin.com/in/ghufran-shahid-a96483241/")}}>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" className="h-7 w-7" />
+                </button>
+                <button onClick={() => {window.open("https://github.com/StretchedCode")}}>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" className="h-7 w-7" />
+                </button>
+            </div>
         </div>
     )
 }
